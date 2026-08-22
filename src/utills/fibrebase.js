c0db54e6d73dafdd25a68d7aff7d7787da57e363
parthp45 <parth78123@gmail.com>
2026-08-22 13:51:20 +0530

    feat: fixing build issue

--- src/utills/fibrebase.js
+++ src/utills/fibrebase.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth();
+const analytics = typeof window !== "undefined" ? getAnalytics(app) : null;
+const auth = getAuth(app);
 
 export { auth, analytics };
